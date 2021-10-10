3e523289e5bf98dfb1779543612f33d52524a6f2
misaraadel <41232116+misaraadel@users.noreply.github.com>
2021-10-10 17:20:36 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    /*start the loading page */
+    window.addEventListener("load", function () {
+        const loader = document.querySelector(".loader");
+        loader.className += " hidden";
+    });
+    //scroll to top make fade for button
+    $(function () {
+        $(document).scroll(function () {
+            var $nav = $(".navbar-fixed-top");
+            $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        });
+    });  
+    //scroll to top make fade for button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 700) {
+            $(".scroll-top-btn").addClass('acitve');
+        } else {
+            $(".scroll-top-btn").removeClass('acitve');
+        }
+    });
+    //scroll to top button
+    $(".scroll-top-btn").on('click', function () {
+        $('html , body').animate({
+            scrollTop: 0
+        }, 100);
+    });
+    //start active navbar
+    $('.hamburger').click(function(){
+        $('.hamburger').toggleClass('active');
+        $('.navbar-nav').toggleClass('active-nav');
+        $('body').toggleClass('overflowNone')
+    });
+    $('.nav-item').click(function(){
+        $('.navbar-nav').toggleClass('active-nav');
+        $('body').removeClass('overflowNone');
+        $('.hamburger').toggleClass('active');
+    });
+   $('.add-popup').click(function(){
+        $('.poup-window').addClass('acitve-popup');
+        $('body').addClass('overflowNone');
+   });
+   $('.close-popup-window').click(function(){
+        $('.poup-window').removeClass('acitve-popup');
+        $('body').removeClass('overflowNone');
+    });
+});
+console.log('version 0.0.1');
